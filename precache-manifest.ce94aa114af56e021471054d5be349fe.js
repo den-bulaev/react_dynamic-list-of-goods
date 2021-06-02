1b7d57d9749f7248160e6f674815e5e67b1384f2
Bulaev Denis <bulaevdenis7@gmail.com>
2021-06-02 15:39:50 +0300

    Updates

---
+++ precache-manifest.ce94aa114af56e021471054d5be349fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb51d4c7a2a13eb983c2aeff83f17a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9e96e8473316a3cb760",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e96e8473316a3cb760",
+    "url": "./static/js/main.417b99da.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
